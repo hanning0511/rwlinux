4476695d43f9ee110b06da124bcf3cf8015bb818
Han Ning <ning.han@intel.com>
2022-08-23 11:57:00 -0400

    move JumpAddr and WriteValue to lib.rs
    Signed-off-by: Han Ning <ning.han@intel.com>

--- src/devmem.rs
+++ src/devmem.rs
@@ -1,6 +1,5 @@
-use super::{Cell, OpMode};
+use super::{Cell, JumpAddress, OpMode, WriteValue};
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
-use hex;
 use libc::{O_RDWR, O_SYNC};
 use memmap::MmapOptions;
 use std::fs::OpenOptions;
@@ -53,115 +52,6 @@ pub fn write(offset: u64, bytes: Vec<u8>) {
     let mut mmap = mmap.unwrap();
 
     mmap.copy_from_slice(&bytes)
-}
-
-struct JumpAddress {
-    inner: String,
-}
-
-impl JumpAddress {
-    fn new(s: &str) -> Self {
-        Self {
-            inner: String::from(s),
-        }
-    }
-
-    fn parse(&self, base: u64) -> Option<u64> {
-        if self.inner.starts_with("+") {
-            let offset = self.inner.strip_prefix("+").unwrap();
-            let offset = u64::from_str_radix(offset, 16);
-            if let Ok(offset) = offset {
-                return Some(base + offset);
-            } else {
-                return None;
-            }
-        } else if self.inner.starts_with("-") {
-            let offset = self.inner.strip_prefix("-").unwrap();
-            let offset = u64::from_str_radix(offset, 16);
-            if let Ok(offset) = offset {
-                if base >= offset {
-                    return Some(base - offset);
-                } else {
-                    return None;
-                }
-            } else {
-                return None;
-            }
-        } else {
-            let offset = u64::from_str_radix(&self.inner, 16);
-            if let Ok(offset) = offset {
-                return Some(offset);
-            } else {
-                return None;
-            }
-        }
-    }
-}
-
-struct WriteValue {
-    inner: String,
-}
-
-impl WriteValue {
-    fn new(s: &str) -> Self {
-        Self {
-            inner: String::from(s),
-        }
-    }
-
-    fn parse(&self) -> Option<Vec<u8>> {
-        if !self.inner.contains(":") {
-            if let Ok(bytes) = hex::decode(&self.inner) {
-                return Some(bytes);
-            } else {
-                return None;
-            }
-        } else {
-            let split_at = self.inner.find(":").unwrap();
-            let prefix = self.inner[0..split_at].to_string();
-            let value = self.inner[split_at + 1..].to_string();
-
-            match prefix.as_str() {
-                "B" => {
-                    if let Ok(byte) = u8::from_str_radix(&value, 16) {
-                        return Some(vec![byte]);
-                    } else {
-                        return None;
-                    }
-                }
-                "W" => {
-                    if let Ok(data) = u16::from_str_radix(&value, 16) {
-                        return Some(data.to_ne_bytes().to_vec());
-                    } else {
-                        return None;
-                    }
-                }
-                "DW" => {
-                    if let Ok(data) = u32::from_str_radix(&value, 16) {
-                        return Some(data.to_ne_bytes().to_vec());
-                    } else {
-                        return None;
-                    }
-                }
-                "QW" => {
-                    if let Ok(data) = u64::from_str_radix(&value, 16) {
-                        return Some(data.to_ne_bytes().to_vec());
-                    } else {
-                        return None;
-                    }
-                }
-                "DQW" => {
-                    if let Ok(data) = u128::from_str_radix(&value, 16) {
-                        return Some(data.to_ne_bytes().to_vec());
-                    } else {
-                        return None;
-                    }
-                }
-                _ => {}
-            }
-        }
-        None
-    }
 }
 
 #[derive(Debug)]
